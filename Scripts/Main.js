d02228630589a0b7c673cf62e251ff72da9222fa
SashaShrek <jmatyushin@gmail.com>
2020-07-13 02:43:57 +0300

    First commit. Start

---
+++ Scripts/Main.js
@@ -0,0 +1,40 @@
+class Loader{
+    Load(){
+        this.Events();
+    }
+    Events(){
+        let slider = new Slider();
+        let i = 0;
+        let n = $(".Gallery > img").length;
+        while(i < n){
+            let elem = $(".Gallery > img")[i];
+            $(".Gallery > img")[i].addEventListener("click", () => slider.Next(elem));
+            i++;
+        }
+        slider.Slide();
+    }
+}
+
+class Slider{
+    Next(val){
+        $(".imgsSlide > img").css({"opacity": 0});
+        setTimeout(() => {
+            $(".imgsSlide > img")[0].src = val.src;
+            $(".imgsSlide > img").css({"opacity": 1});
+        }, 1000);
+    }
+    Slide(){
+        let n = $(".Gallery > img").length;
+        let i = 1;
+        
+        setInterval(() => {
+            let elem = $(".Gallery > img")[i];
+            $(".imgsSlide > img").css({"opacity": 0});
+            setTimeout(() => {
+                $(".imgsSlide > img")[0].src = elem.src;
+                $(".imgsSlide > img").css({"opacity": 1});
+            }, 1000);
+            i = i == n - 1 ? 0 : i + 1;
+        }, 6000)
+    }
+}
