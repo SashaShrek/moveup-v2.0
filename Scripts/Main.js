3d73360b53739e20fbf4c1d66f1c5fa46d4a0ea3
SashaShrek <jmatyushin@gmail.com>
2020-07-24 23:38:03 +0300

    Used typescript

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,62 +1,74 @@
 'use strict';
-class Loader{
-    Load(){
-        this.Events();
+var Loader = /** @class */ (function () {
+    function Loader() {
     }
-    Events(){
-    //slider
-        let slider = new Slider();
-        let i = 0;
-        let n = $(".Gallery > img").length;
-        while(i < n){
-            let elem = $(".Gallery > img")[i];
-            $(".Gallery > img")[i].addEventListener("click", () => slider.Next(elem));
+    Loader.prototype.Load = function () {
+        this.Events();
+    };
+    Loader.prototype.Events = function () {
+        //slider
+        var slider = new Slider();
+        var i = 0;
+        var n = $(".Gallery > img").length;
+        var _loop_1 = function () {
+            var elem = $(".Gallery > img")[i];
+            $(".Gallery > img")[i].addEventListener("click", function () { return slider.Next(elem); });
             i++;
+        };
+        while (i < n) {
+            _loop_1();
         }
         slider.Slide();
-    //navigator
-        let navigator = new Navigator();
-        let data = [
+        //navigator
+        var navigator = new Navigat();
+        var data = [
             "hoster_block.html",
             "help.html",
             "busket.html",
             "input.html"
         ];
         i = 0;
-        while(i <= 3){
-            let page = data[i];
-            $("#root > div > header > div > button")[i].addEventListener("click", () => navigator.Go(page));
+        var _loop_2 = function () {
+            var page = data[i];
+            $("#root > div > header > div > button")[i].addEventListener("click", function () { return navigator.Go(page); });
             i++;
+        };
+        while (i <= 3) {
+            _loop_2();
         }
+    };
+    return Loader;
+}());
+var Slider = /** @class */ (function () {
+    function Slider() {
     }
-}
-
-class Slider{
-    Next(val){
-        $(".imgsSlide > img").css({"opacity": 0.2});
-        setTimeout(() => {
+    Slider.prototype.Next = function (val) {
+        $(".imgsSlide > img").css({ "opacity": 0.2 });
+        setTimeout(function () {
             $(".imgsSlide > img")[0].src = val.src;
-            $(".imgsSlide > img").css({"opacity": 1});
-        }, 500);//400
-    }
-    Slide(){
-        let n = $(".Gallery > img").length;
-        let i = 1;
-        
-        setInterval(() => {
-            let elem = $(".Gallery > img")[i];
-            $(".imgsSlide > img").css({"opacity": 0.2});
-            setTimeout(() => {
+            $(".imgsSlide > img").css({ "opacity": 1 });
+        }, 500); //400
+    };
+    Slider.prototype.Slide = function () {
+        var n = $(".Gallery > img").length;
+        var i = 1;
+        setInterval(function () {
+            var elem = $(".Gallery > img")[i];
+            $(".imgsSlide > img").css({ "opacity": 0.2 });
+            setTimeout(function () {
                 $(".imgsSlide > img")[0].src = elem.src;
-                $(".imgsSlide > img").css({"opacity": 1});
+                $(".imgsSlide > img").css({ "opacity": 1 });
             }, 500);
             i = i == n - 1 ? 0 : i + 1;
-        }, 5000)
+        }, 5000);
+    };
+    return Slider;
+}());
+var Navigat = /** @class */ (function () {
+    function Navigat() {
     }
-}
-
-class Navigator{
-    Go(page){
+    Navigat.prototype.Go = function (page) {
         document.location.href = page;
-    }
-}
+    };
+    return Navigat;
+}());
---
+++ Scripts/Main.ts
@@ -0,0 +1,63 @@
+'use strict';
+
+class Loader{
+    Load(){
+        this.Events();
+    }
+    Events(): void{
+        //slider
+            let slider: Slider = new Slider();
+            let i: number = 0;
+            let n: number = $(".Gallery > img").length;
+            while(i < n){
+                let elem: HTMLElement = $(".Gallery > img")[i];
+                $(".Gallery > img")[i].addEventListener("click", () => slider.Next(elem));
+                i++;
+            }
+            slider.Slide();
+        //navigator
+            let navigator: Navigat = new Navigat();
+            let data: object = [
+                "hoster_block.html",
+                "help.html",
+                "busket.html",
+                "input.html"
+            ];
+            i = 0;
+            while(i <= 3){
+                let page: string = data[i];
+                $("#root > div > header > div > button")[i].addEventListener("click", () => navigator.Go(page));
+                i++;
+            }
+        }
+}
+
+class Slider{
+    Next(val: HTMLElement): void{
+        $(".imgsSlide > img").css({"opacity": 0.2});
+        setTimeout(() => {
+            ($(".imgsSlide > img")[0] as HTMLImageElement).src = (val as HTMLImageElement).src;
+            $(".imgsSlide > img").css({"opacity": 1});
+        }, 500);//400
+    }
+    Slide(): void{
+        let n: number = $(".Gallery > img").length;
+        let i: number = 1;
+        
+        setInterval(() => {
+            let elem: HTMLElement = $(".Gallery > img")[i];
+            $(".imgsSlide > img").css({"opacity": 0.2});
+            setTimeout(() => {
+                ($(".imgsSlide > img")[0] as HTMLImageElement).src = (elem as HTMLImageElement).src;
+                $(".imgsSlide > img").css({"opacity": 1});
+            }, 500);
+            i = i == n - 1 ? 0 : i + 1;
+        }, 5000)
+    }
+}
+
+class Navigat{
+    Go(page: string): void{
+        document.location.href = page;
+    }
+}
